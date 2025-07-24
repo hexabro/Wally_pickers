d18488a6f08a097d4ed5eb5c7e7587aba42b18b3
Pablo <lpz.pablo.16@gmail.com>
2025-07-24 14:09:39 +0200

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginReact, { rules } from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-    plugins: {
-      js,
-      "@typescript-eslint": tseslint,
-      react: pluginReact,
-    },
-    extends: [
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
-    ],
-    rules: {
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
-  
+
+module.exports = {
+  rules: {
+    "react/jsx-uses-react": "off",
+  }
+}
