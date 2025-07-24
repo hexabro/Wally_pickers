5038f4a85f5305d88d28eb33d0c0edde3651bef1
Pablo <lpz.pablo.16@gmail.com>
2025-07-24 13:39:00 +0200

    please vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,34 +2,20 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import pluginNext from "eslint-plugin-next";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: {
-      js,
-      react: pluginReact,
-      next: pluginNext,
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      "react/jsx-uses-react": 0,
-      "react/react-in-jsx-scope": 0,
-    },
-    languageOptions: {
-      globals: globals.browser,
-    },
-    extends: [
-      "js/recommended",
-      pluginNext.configs.recommended,
-    ],
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ]);
+
+module.exports = {
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+
+  }
+}
