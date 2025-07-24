cbd0d632ffd6a76dd3abf573da0fe83c5cbc6002
Pablo <lpz.pablo.16@gmail.com>
2025-07-24 13:27:49 +0200

    si

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-// eslint.config.mjs
-
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginNext from "eslint-plugin-next";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -12,6 +11,12 @@ export default defineConfig([
     plugins: {
       js,
       react: pluginReact,
+      next: pluginNext,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
     rules: {
       "react/jsx-uses-react": 0,
@@ -20,7 +25,10 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
-    extends: ["js/recommended"],
+    extends: [
+      "js/recommended",
+      pluginNext.configs.recommended,
+    ],
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
