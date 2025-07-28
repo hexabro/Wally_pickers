6219c03ecd130dc6979ff638742fd71e9f049934
Pablo <lpz.pablo.16@gmail.com>
2025-07-28 19:23:33 +0200

    client mail

--- src/components/nav/CartSidebar.tsx
+++ src/components/nav/CartSidebar.tsx
@@ -117,7 +117,7 @@ const CartSidebar = ({ isOpen, setIsOpen }: propsType) => {
               `${item.product.REF} - ${item.product.NOMBRE} (${item.product.MARCA}) - ${item.quantity} cajas`
             ).join('\n');
 
-            const endpoint = "https://script.google.com/macros/s/AKfycbyj856qCYLOjnpplmkdZRzwYUuEBSQ93kfT0C8RxGCCxnCmHuau3MzuG9LBPMFyRilQ/exec"
+            const endpoint = "https://script.google.com/macros/s/AKfycbwHF4fdsZMGbcyzc05tkNH56nSBw42RwdVi9us_jWdhZPsPCm1r_1_KmeTSg1v-SGXjnw/exec"
 
 
 
